f0c3b005eee3b05fc1d8eb05669af4b4f37f0cb3
torbjorn <torbjorn>
2005-05-09 11:12:17 +0000

    Added support for command line options. Added command line parameters: -classpath -sourcepath -bootclasspath -extdirs

--- Java1.4Frontend/JavaPrettyPrinter.java
+++ Java1.4Frontend/JavaPrettyPrinter.java
@@ -1,37 +1,37 @@
 import AST.*;
 
+import java.util.*;
 import java.io.*;
 import parser.*;
 
 class JavaPrettyPrinter {
 
   public static void main(String args[]) {
-    String[] files = args;
-    if(files.length == 1 && new File(files[0]).isDirectory()) {
-      String dirName = files[0];
-      if(!dirName.endsWith("/"))
-        dirName += "/";
-      File file = new File(dirName);
-      files = file.list(new FilenameFilter() {
-        public boolean accept(File dir, String name) {
-          return name.endsWith(".java");
-        }
-      });
-      for(int i = 0; i < files.length; i++) {
-        files[i] = dirName + files[i];
-      }
-    }
     Program program = new Program();
+    program.addOptionDescription("-classpath", true);
+    program.addOptionDescription("-sourcepath", true);
+    program.addOptionDescription("-bootclasspath", true);
+    program.addOptionDescription("-extdirs", true);
+    program.addOptionDescription("-d", true);
+    program.addOptionDescription("-verbose", false);
+    
+    program.addOptions(args);
+    Collection files = program.files();
+    
     long initParseTime = System.currentTimeMillis();
     program = new Program();
     JavaParser parser = new JavaParser();
     initParseTime = System.currentTimeMillis() - initParseTime;
     long parseTime = System.currentTimeMillis();
-    for(int i = 0; i < files.length; i++) {
+    for(Iterator iter = files.iterator(); iter.hasNext(); ) {
       try {
-        Reader reader = new FileReader(files[i]);
+        String name = (String)iter.next();
+        Reader reader = new FileReader(name);
         JavaScanner scanner = new JavaScanner(new UnicodeEscapes(new BufferedReader(reader)));
         CompilationUnit unit = ((Program)parser.parse(scanner)).getCompilationUnit(0);
+        unit.setFromSource(true);
+        unit.setRelativeName(name);
+        unit.setPathName(".");
       	reader.close();
         
         //long parse = System.currentTimeMillis();
@@ -45,11 +45,11 @@ class JavaPrettyPrinter {
     }
     parseTime = System.currentTimeMillis() - parseTime;
     long rewriteAndUpdateTime = System.currentTimeMillis();
-    program.updateRemoteAttributeWrites(files.length);
+    program.updateRemoteAttributeWrites(files.size());
     rewriteAndUpdateTime = System.currentTimeMillis() - rewriteAndUpdateTime;
-    //program.prettyPrint(files.length);
+    program.prettyPrint(files.size());
     long errorCheckTime = System.currentTimeMillis();
-    if(program.errorCheck(files.length)) {
+    if(program.errorCheck(files.size())) {
       errorCheckTime = System.currentTimeMillis() - errorCheckTime;
       //long code = System.currentTimeMillis() - start - program.parseTime;
       System.err.println("InitParser: " + initParseTime);
@@ -58,7 +58,7 @@ class JavaPrettyPrinter {
       System.err.println("String computation: " + (program.stringComputation - program.classFileReadTime));
       System.err.println("Print time: " + program.printTime);
       System.err.println("Error check time: " + errorCheckTime);
-      System.err.println("Number of files: " + files.length);
+      System.err.println("Number of files: " + files.size());
       
       System.exit(1);
       
@@ -71,7 +71,7 @@ class JavaPrettyPrinter {
     System.err.println("String computation: " + (program.stringComputation - program.classFileReadTime));
     System.err.println("Print time: " + program.printTime);
     System.err.println("Error check time: " + errorCheckTime);
-    System.err.println("Number of files: " + files.length);
+    System.err.println("Number of files: " + files.size());
   }
 
   
