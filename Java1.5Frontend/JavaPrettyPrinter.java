214d610732a34f0d7685d27596d846bace1cd345
torbjorn <torbjorn>
2005-05-11 07:55:31 +0000

    Added support for command line parameters and new remote attribute collection update strategy.

---
+++ Java1.5Frontend/JavaPrettyPrinter.java
@@ -0,0 +1,78 @@
+import AST.*;
+
+import java.util.*;
+import java.io.*;
+import parser.*;
+
+class JavaPrettyPrinter {
+
+  public static void main(String args[]) {
+    Program program = new Program();
+    program.addKeyValueOption("-classpath");
+    program.addKeyValueOption("-sourcepath");
+    program.addKeyValueOption("-bootclasspath");
+    program.addKeyValueOption("-extdirs");
+    program.addKeyValueOption("-d");
+    program.addKeyOption("-verbose");
+    
+    program.addOptions(args);
+    Collection files = program.files();
+    
+    long initParseTime = System.currentTimeMillis();
+    program = new Program();
+    JavaParser parser = new JavaParser();
+    initParseTime = System.currentTimeMillis() - initParseTime;
+    long parseTime = System.currentTimeMillis();
+    for(Iterator iter = files.iterator(); iter.hasNext(); ) {
+      try {
+        String name = (String)iter.next();
+        Reader reader = new FileReader(name);
+        JavaScanner scanner = new JavaScanner(new UnicodeEscapes(new BufferedReader(reader)));
+        CompilationUnit unit = ((Program)parser.parse(scanner)).getCompilationUnit(0);
+        unit.setFromSource(true);
+        unit.setRelativeName(name);
+        unit.setPathName(".");
+      	reader.close();
+        
+        //long parse = System.currentTimeMillis();
+        program.addCompilationUnit(unit);
+        //program.parseTime += System.currentTimeMillis() - parse;
+      } catch (IOException e) {
+      } catch (Exception e) {
+        System.err.println(e);
+        e.printStackTrace();
+      }
+    }
+    parseTime = System.currentTimeMillis() - parseTime;
+    long rewriteAndUpdateTime = System.currentTimeMillis();
+    program.updateRemoteAttributeCollections(files.size());
+    rewriteAndUpdateTime = System.currentTimeMillis() - rewriteAndUpdateTime;
+    program.prettyPrint(files.size());
+    long errorCheckTime = System.currentTimeMillis();
+    if(program.errorCheck(files.size())) {
+      errorCheckTime = System.currentTimeMillis() - errorCheckTime;
+      //long code = System.currentTimeMillis() - start - program.parseTime;
+      System.err.println("InitParser: " + initParseTime);
+      System.err.println("Parse: " + parseTime);
+      System.err.println("ClassFileReadTime: " + program.classFileReadTime);
+      System.err.println("String computation: " + (program.stringComputation - program.classFileReadTime));
+      System.err.println("Print time: " + program.printTime);
+      System.err.println("Error check time: " + errorCheckTime);
+      System.err.println("Number of files: " + files.size());
+      
+      System.exit(1);
+      
+    }
+    errorCheckTime = System.currentTimeMillis() - errorCheckTime;
+    //long code = System.currentTimeMillis() - start - program.parseTime;
+    System.err.println("InitParser: " + initParseTime);
+    System.err.println("Parse: " + parseTime);
+    System.err.println("ClassFileReadTime: " + program.classFileReadTime);
+    System.err.println("String computation: " + (program.stringComputation - program.classFileReadTime));
+    System.err.println("Print time: " + program.printTime);
+    System.err.println("Error check time: " + errorCheckTime);
+    System.err.println("Number of files: " + files.size());
+  }
+
+  
+}
---
+++ Java1.5Frontend/TestCompiler.java
@@ -0,0 +1,51 @@
+import AST.*;
+
+import java.util.*;
+import java.io.*;
+import parser.*;
+
+class TestCompiler {
+
+  public static void main(String args[]) {
+    Program program = new Program();
+    program.addOptionDescription("-classpath", true);
+    program.addOptionDescription("-sourcepath", true);
+    program.addOptionDescription("-bootclasspath", true);
+    program.addOptionDescription("-extdirs", true);
+    program.addOptionDescription("-d", true);
+    program.addOptionDescription("-verbose", false);
+    
+    program.addOptions(args);
+    Collection files = program.files();
+    
+    JavaParser parser = new JavaParser();
+    for(Iterator iter = files.iterator(); iter.hasNext(); ) {
+      try {
+        String name = (String)iter.next();
+        Reader reader = new FileReader(name);
+        JavaScanner scanner = new JavaScanner(new UnicodeEscapes(new BufferedReader(reader)));
+        CompilationUnit unit = ((Program)parser.parse(scanner)).getCompilationUnit(0);
+        unit.setFromSource(true);
+        unit.setRelativeName(name);
+        unit.setPathName(".");
+      	reader.close();
+        program.addCompilationUnit(unit);
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        return;
+      } catch (IOException e) {
+        System.err.println(e);
+        e.printStackTrace();
+        return;
+      } catch (Exception e) {
+        System.err.println(e);
+        e.printStackTrace();
+        return;
+      }
+    }
+    program.updateRemoteAttributeCollections(files.size());
+    if(program.errorCheck(files.size())) {
+      System.exit(1);
+    }
+  }
+}
