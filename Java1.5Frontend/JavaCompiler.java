d43002e4ce9f91162c9addb9f6c4325236daf760
torbjorn <torbjorn>
2006-07-29 21:08:33 +0000

    *** empty log message ***

--- Java1.5Frontend/JavaCompiler.java
+++ Java1.5Frontend/JavaCompiler.java
@@ -95,7 +95,7 @@ class JavaCompiler {
   }
 
   protected static void printVersion() {
-    System.out.println("Java1.4Frontend + Backend (http://jastadd.cs.lth.se) Version R20060127");
+    System.out.println("Java1.4Frontend + Backend + Java5 extensions (http://jastadd.cs.lth.se) Version R20060729");
   }
 
 }
