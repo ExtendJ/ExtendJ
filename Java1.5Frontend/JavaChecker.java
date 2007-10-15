bd4394aeaab1f59499e9d2ea0b26778bba36b6b8
torbjorn <torbjorn>
2007-10-15 11:28:20 +0000

    *** empty log message ***

---
+++ Java1.5Frontend/JavaChecker.java
@@ -0,0 +1,22 @@
+import AST.*;
+
+class JavaChecker extends Frontend {
+  public static void main(String args[]) {
+    compile(args);
+  }
+
+  public static boolean compile(String args[]) {
+    return new JavaChecker().process(
+        args,
+        new bytecode.Parser(),
+        new JavaParser() {
+          public CompilationUnit parse(java.io.InputStream is, String fileName) throws java.io.IOException, beaver.Parser.Exception {
+            return new parser.JavaParser().parse(is, fileName);
+          }
+        }
+    );
+  }
+
+  protected String name() { return "Java1.4Frontend + Java1.5Extensions"; }
+  protected String version() { return "R20060915"; }
+}
