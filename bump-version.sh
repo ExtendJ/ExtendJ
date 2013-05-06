b97ae03d049982b4be74650fb23d7a23cff309cb
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-05-06 14:10:33 +0200

    Improved version bumping script

--- bump-version.sh
+++ bump-version.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+VERSION=`git describe`
+SHORTVERSION=`git describe --abbrev=0`
+
 # bump version strings using git describe
 echo "Bumping JastAddJ version strings..."
-echo "version=`git describe`" > src/res/JastAddJ.properties
-echo "version.short=`git describe --abbrev=0`" >> src/res/JastAddJ.properties
-echo "Don't forget to commit these changes!"
+echo "version=$VERSION"
+echo "version=$VERSION" > src/res/JastAddJ.properties
+echo "version.short=$SHORTVERSION"
+echo "version.short=$SHORTVERSION" >> src/res/JastAddJ.properties
