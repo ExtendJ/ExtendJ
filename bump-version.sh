1b9a3f019d3f2137e8715f557c8cb970cbf9ea59
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-05-07 09:50:55 +0200

    Replaced bump-version script with Ant target

--- bump-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VERSION=`git describe`
-
-# bump version string using git describe
-echo "Bumping JastAddJ version string..."
-echo "version=$VERSION"
-echo "version=$VERSION" > src/res/JastAddJ.properties
