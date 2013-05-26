49f2b3cd7179f2fef3b50d916d07de0247a4828f
Jesper Öqvist <jesper@llbit.se>
2013-05-26 21:30:06 +0200

    Sloccount separates frontend/backend

--- sloccount.sh
+++ sloccount.sh
@@ -21,10 +21,19 @@ for module in java*; do
 	find $module -name '*.ast' > $tempfile
 	java_count -f $tempfile | tail -n 1
 
-	printf "  jrag: "
-	find $module -name '*.jadd' > $tempfile
-	find $module -name '*.jrag' >> $tempfile
+	printf "  frontend: "
+	find $module/frontend -name '*.jadd' > $tempfile
+	find $module/frontend -name '*.jrag' >> $tempfile
 	java_count -f $tempfile | tail -n 1
+
+	printf "  backend: "
+	if [ -d "$module/backend" ]; then
+		find $module/backend -name '*.jadd' > $tempfile
+		find $module/backend -name '*.jrag' >> $tempfile
+		java_count -f $tempfile | tail -n 1
+	else
+		printf "0\n"
+	fi
 
 	printf "  scanner: "
 	find $module -name '*.flex' > $tempfile
