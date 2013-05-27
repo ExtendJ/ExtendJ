b6af29967b6ac332b6c614be276ccf8c33112040
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-05-27 15:00:45 +0200

    Updated SLOC counting script

--- sloccount.sh
+++ sloccount.sh
@@ -5,12 +5,12 @@
 
 tempfile=files-$$.tmp
 echo "generic"
-printf "  java: "
+printf "    java: "
 find src/java/org/jastadd/jastaddj/ -name '*.java' > $tempfile
 java_count -f $tempfile | tail -n 1
 
 echo "generated"
-printf "  java: "
+printf "    java: "
 find src/gen/ -name '*.java' > $tempfile
 java_count -f $tempfile | tail -n 1
 
@@ -24,34 +24,7 @@ for module in java*; do
 	echo $module
 	moduletot=0
 
-	printf "  grammar: "
-	find $module -name '*.ast' > $tempfile
-	num=`java_count -f $tempfile | tail -n 1`
-	grammar=`expr $grammar + $num`
-	moduletot=`expr $moduletot + $num`
-	echo $num
-
-	printf "  frontend: "
-	find $module/frontend -name '*.jadd' > $tempfile
-	find $module/frontend -name '*.jrag' >> $tempfile
-	num=`java_count -f $tempfile | tail -n 1`
-	frontend=`expr $frontend + $num`
-	moduletot=`expr $moduletot + $num`
-	echo $num
-
-	printf "  backend: "
-	if [ -d "$module/backend" ]; then
-		find $module/backend -name '*.jadd' > $tempfile
-		find $module/backend -name '*.jrag' >> $tempfile
-		num=`java_count -f $tempfile | tail -n 1`
-	else
-		num=0
-	fi
-	backend=`expr $backend + $num`
-	moduletot=`expr $moduletot + $num`
-	echo $num
-
-	printf "  scanner: "
+	printf "    scanner: "
 	find $module -name '*.flex' > $tempfile
 	numfiles=`wc -l $tempfile | awk '{print $1}'`
 	if [ "$numfiles" -gt "0" ]; then
@@ -63,21 +36,49 @@ for module in java*; do
 	moduletot=`expr $moduletot + $num`
 	echo $num
 
-	printf "  parser: "
+	printf "    parser: "
 	find $module -name '*.parser' > $tempfile
 	num=`java_count -f $tempfile | tail -n 1`
 	parser=`expr $parser + $num`
 	moduletot=`expr $moduletot + $num`
 	echo $num
 
-	echo "  total: $moduletot"
+	printf "    grammar: "
+	find $module -name '*.ast' > $tempfile
+	num=`java_count -f $tempfile | tail -n 1`
+	grammar=`expr $grammar + $num`
+	moduletot=`expr $moduletot + $num`
+	echo $num
+
+	printf "    frontend: "
+	find $module/frontend -name '*.jadd' > $tempfile
+	find $module/frontend -name '*.jrag' >> $tempfile
+	num=`java_count -f $tempfile | tail -n 1`
+	frontend=`expr $frontend + $num`
+	moduletot=`expr $moduletot + $num`
+	echo $num
+
+	printf "    backend: "
+	if [ -d "$module/backend" ]; then
+		find $module/backend -name '*.jadd' > $tempfile
+		find $module/backend -name '*.jrag' >> $tempfile
+		num=`java_count -f $tempfile | tail -n 1`
+	else
+		num=0
+	fi
+	backend=`expr $backend + $num`
+	moduletot=`expr $moduletot + $num`
+	echo $num
+
+	echo "    all: $moduletot"
 done
 
-echo "total"
-echo "  grammar: $grammar"
-echo "  frontend: $frontend"
-echo "  backend: $backend"
-echo "  scanner: $scanner"
-echo "  parser: $parser"
+echo "totals:"
+echo "    scanner: $scanner"
+echo "    parser: $parser"
+echo "    grammar: $grammar"
+echo "    frontend: $frontend"
+echo "    backend: $backend"
+echo "    all: " `expr $grammar + $frontend + $backend + $scanner + $parser`
 
 rm $tempfile
