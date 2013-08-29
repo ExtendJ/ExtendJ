6cbe31c952f7fbe1d6826d2e6e9a8de40f43cb0f
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-08-29 11:30:05 +0200

    Added missing license headers

--- src/java/org/jastadd/util/RobustLinkedList.java
+++ src/java/org/jastadd/util/RobustLinkedList.java
@@ -1,21 +1,48 @@
+/* Copyright (c) 2013, Jesper Öqvist <jesper.oqvist@cs.lth.se>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Lund University nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.jastadd.util;
 import java.util.Collection;
 import java.util.Iterator;
 
 /**
  * A robust linked list that can be iterated while elements are added.
- * 
+ *
  * The list does not support concurrent (threaded) modification,
  * but simultaneous (non-threaded) modification is supported.
- * 
+ *
  * The robust linked list always keeps a tail node, which the iterator
  * points to when it has run past the end.
- * 
+ *
  * @author Jesper Öqvist <jesper.oqvist@cs.lth.se>
  * @param <V> The list item type
  */
 public class RobustLinkedList<V> implements Collection<V> {
-	
+
 	private static final class Node<V> {
 		public V datum;
 		Node<V> succ = null;
@@ -24,7 +51,7 @@ public class RobustLinkedList<V> implements Collection<V> {
 			this.datum = v;
 		}
 	}
-	
+
 	/**
 	 * A robust linked list iterator.
 	 * @author Jesper Öqvist <jesper.oqvist@cs.lth.se>
@@ -33,16 +60,16 @@ public class RobustLinkedList<V> implements Collection<V> {
 	public class RobustIterator<I> implements Iterator<I> {
 
 		private Node<I> ptr;
-		
+
 		/**
 		 * Build a robust iterator for the list.
-		 * @param head 
+		 * @param head
 		 */
 		public RobustIterator(Node<I> head) {
 			ptr = new Node<I>(null);
 			ptr.succ = head;
 		}
-		
+
 		@Override
 		public boolean hasNext() {
 			return ptr.succ.succ != null;
@@ -59,24 +86,24 @@ public class RobustLinkedList<V> implements Collection<V> {
 		public void remove() {
 			Node<I> pred = ptr.succ.pred;
 			Node<I> succ = ptr.succ.succ;
-			
+
 			if (pred != null)
 				pred.succ = succ;
-			
+
 			if (succ != null) {
 				succ.pred = pred;
 				size -= 1;
 			}
-			
+
 			ptr.succ = succ;
 		}
 	}
-	
+
 	/**
 	 * Head node
 	 */
 	private Node<V> head = new Node<V>(null);
-	
+
 	/**
 	 * Empty tail node. Not part of the list.
 	 */
