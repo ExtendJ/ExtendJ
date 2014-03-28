495441245f584c6d3ec68dd11984721291284213
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2014-03-28 16:22:15 +0100

    Merged module definition files

--- buildSrc/src/main/groovy/ModuleLoader.groovy
+++ buildSrc/src/main/groovy/ModuleLoader.groovy
@@ -1,8 +1,9 @@
 class ModuleLoader {
-	static void load(dir, name) {
+	static void load(dir) {
+		def name = "modules"
 		def source = new File(dir, name)
 		if (!source.exists()) {
-			throw new Error("Could not find module: ${dir}/${name}")
+			throw new Error("Could not load module definitions: ${dir}/${name}")
 		}
 		def code = source.text
 		def closure = new GroovyShell().evaluate("{->${code}}")
