67a613912cede1d3a287a0dd1c6d74af5fcfb16f
Jesper Öqvist <joqvist@google.com>
2016-07-25 10:23:46 -0700

    Fix error in labeled statement pretty printing
    Labeled statements now pretty-print their statement, not only the label.
    
    fixes #177 (bitbucket)

--- pretty-print/gen.sh
+++ pretty-print/gen.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Re-generates the pretty printing aspect code.
 
 set -eu
 
-cp ../../aspectgen/aspectgen.jar .
+ASPECTGEN="../../aspectgen/aspectgen.jar"
+if [ ! -e "$ASPECTGEN" ]; then
+    echo "Error: $ASPECTGEN is missing!"
+    exit 1
+fi
 for JAVA_VERSION in {4,5,7,8}; do
-	java -jar aspectgen.jar -aspect "Java${JAVA_VERSION}PrettyPrint" Header.tt \
+	java -jar "$ASPECTGEN" -aspect "Java${JAVA_VERSION}PrettyPrint" Header.tt \
 		"Java${JAVA_VERSION}PrettyPrint.tt" > \
 		"../java${JAVA_VERSION}/frontend/PrettyPrint.jadd"
 done
