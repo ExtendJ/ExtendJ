47d7ac34b0a49442cfef8a2d64c2eb62522b4aaa
Jesper Öqvist <jesper.oqvist@gmail.com>
2026-08-24 20:52:23 +0200

    Include invocation type constraints in nested bound sets

---
+++ rtest/tests/ti/lambda_self_bound_02p/Test.java
@@ -0,0 +1,9 @@
+// Nested lambda of same type passing a type down and then up the call chain.
+// .result: COMPILE_PASS
+public abstract class Test {
+  abstract <I, O> O lift(Zap<I, O> z, I i);
+   { lift(u -> lift(v -> lift(w -> w, v), u), "wIrnrnkajPM").length(); }
+}
+interface Zap<L, R> {
+  R zap(L t);
+}
